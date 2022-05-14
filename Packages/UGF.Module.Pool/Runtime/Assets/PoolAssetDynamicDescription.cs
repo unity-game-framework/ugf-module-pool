94aa02306c899ef2aaf96bdeb6d3adeda5c4f406
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-14 20:37:28 +0200

    Add PoolAssetDynamicDescription

---
+++ Packages/UGF.Module.Pool/Runtime/Assets/PoolAssetDynamicDescription.cs
@@ -0,0 +1,13 @@
+﻿namespace UGF.Module.Pool.Runtime.Assets
+{
+    public class PoolAssetDynamicDescription : PoolAssetDescription
+    {
+        public int DefaultCount { get; set; } = 4;
+        public bool ExpandEnable { get; set; } = true;
+        public int ExpandCount { get; set; } = 4;
+        public int ExpandThreshold { get; set; } = 0;
+        public bool TrimEnable { get; set; } = true;
+        public int TrimCount { get; set; } = 4;
+        public int TrimThreshold { get; set; } = 8;
+    }
+}
