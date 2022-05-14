4bf563959d4bd6febf35f2a51bbe0189d7180630
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-14 20:07:20 +0200

    Add PoolLoader`1

---
+++ Packages/UGF.Module.Pool/Runtime/PoolLoader`1.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using UGF.Pool.Runtime;
+using UGF.RuntimeTools.Runtime.Contexts;
+
+namespace UGF.Module.Pool.Runtime
+{
+    public abstract class PoolLoader<TCollection, TDescription> : PoolLoader
+        where TCollection : class, IPoolCollection
+        where TDescription : class, IPoolDescription
+    {
+        protected override IPoolCollection OnLoad(IPoolDescription description, IContext context)
+        {
+            return OnLoad((TDescription)description, context);
+        }
+
+        protected override async Task<IPoolCollection> OnLoadAsync(IPoolDescription description, IContext context)
+        {
+            return await OnLoadAsync((TDescription)description, context);
+        }
+
+        protected override void OnUnload(IPoolCollection collection, IPoolDescription description, IContext context)
+        {
+            OnUnload((TCollection)collection, (TDescription)description, context);
+        }
+
+        protected override Task OnUnloadAsync(IPoolCollection collection, IPoolDescription description, IContext context)
+        {
+            return OnUnloadAsync((TCollection)collection, (TDescription)description, context);
+        }
+
+        protected abstract TCollection OnLoad(TDescription description, IContext context);
+        protected abstract Task<TCollection> OnLoadAsync(TDescription description, IContext context);
+        protected abstract void OnUnload(TCollection collection, TDescription description, IContext context);
+        protected abstract Task OnUnloadAsync(TCollection collection, TDescription description, IContext context);
+    }
+}
