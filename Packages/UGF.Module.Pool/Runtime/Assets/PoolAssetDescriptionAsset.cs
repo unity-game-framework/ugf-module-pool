278188424865bac72365158a81760039f12cd62c
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-15 15:11:44 +0200

    Update PoolAssetDescriptionAsset.cs

--- Packages/UGF.Module.Pool/Runtime/Assets/PoolAssetDescriptionAsset.cs
+++ Packages/UGF.Module.Pool/Runtime/Assets/PoolAssetDescriptionAsset.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 namespace UGF.Module.Pool.Runtime.Assets
 {
-    public abstract class PoolAssetDescriptionAsset : PoolDescriptionAsset
+    [CreateAssetMenu(menuName = "Unity Game Framework/Pool/Pool Asset Description", order = 2000)]
+    public class PoolAssetDescriptionAsset : PoolDescriptionAsset
     {
         [AssetGuid(typeof(Object))]
         [SerializeField] private string m_asset;
@@ -13,5 +14,16 @@ namespace UGF.Module.Pool.Runtime.Assets
         public string Asset { get { return m_asset; } set { m_asset = value; } }
         public int Count { get { return m_count; } set { m_count = value; } }
         public int Capacity { get { return m_capacity; } set { m_capacity = value; } }
+
+        protected override IPoolDescription OnBuild()
+        {
+            return new PoolAssetDescription
+            {
+                LoaderId = Loader,
+                AssetId = m_asset,
+                Count = m_count,
+                Capacity = m_capacity
+            };
+        }
     }
 }
