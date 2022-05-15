88260f1cd13d78b0f4b636203064d29965e94d59
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-15 16:09:12 +0200

    Add TestPoolModule

---
+++ Assets/UGF.Module.Pool.Runtime.Tests/TestPoolModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using UGF.Application.Runtime;
+
+namespace UGF.Module.Pool.Runtime.Tests
+{
+    public class TestPoolModule
+    {
+        [Test]
+        public async Task LoadAndUnload()
+        {
+            var launcher = new ApplicationLauncherResources("Launcher");
+
+            await launcher.CreateAsync();
+
+            var module = launcher.Application.GetModule<PoolModule>();
+
+            launcher.Destroy();
+        }
+    }
+}
