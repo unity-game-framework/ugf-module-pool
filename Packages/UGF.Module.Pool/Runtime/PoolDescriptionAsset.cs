0d41723c6ee6f9b5e7315fa37ae58fe1f8c38b1c
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-15 15:00:05 +0200

    Update PoolDescriptionAsset.cs

--- Packages/UGF.Module.Pool/Runtime/PoolDescriptionAsset.cs
+++ Packages/UGF.Module.Pool/Runtime/PoolDescriptionAsset.cs
@@ -1,8 +1,14 @@
 ﻿using UGF.Builder.Runtime;
+using UGF.EditorTools.Runtime.IMGUI.Attributes;
+using UnityEngine;
 
 namespace UGF.Module.Pool.Runtime
 {
     public abstract class PoolDescriptionAsset : BuilderAsset<IPoolDescription>
     {
+        [AssetGuid(typeof(PoolLoaderAsset))]
+        [SerializeField] private string m_loader;
+
+        public string Loader { get { return m_loader; } set { m_loader = value; } }
     }
 }
